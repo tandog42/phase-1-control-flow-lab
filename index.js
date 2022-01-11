4db95d8d2bec28254c2ea6d4a4d0441fa53404f3
Tanner <troxs690@gmail.com>
2022-01-11 11:07:20 -0600

    Done

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+
+  function scuberGreetingForFeet(distance){
+    if(distance <= 400){
+      return 'This one is on me!'; }
+
+      else if( distance >= 2000 && distance<=2500) 
+      {
+      return 'I will gladly take your thirty bucks.'}
+      
+      else return 'No can do.' 
+}
   // Write your code here!
+
+
+function ternaryCheckCity(Y){
+  if(Y==="NYC"){
+  return 'Ok, sounds good.' }
+  else return 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  if(tip==="generous")
+  {
+    return 'Thank you so much.' }
+    
+    else if(tip==="not as generous")
+    { return 'Thank you.' }
+    else return 'Bye.'
+  }
